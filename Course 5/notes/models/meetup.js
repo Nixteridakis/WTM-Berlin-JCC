feff43fba58fd266ed1efa1a003fd17667c07aa3
Marinos Chrysanthou <marinoschrysanthou@Marinoss-MacBook-Pro.local>
2018-11-26 19:29:05 +0100

    removed notes

--- Course 5/notes/models/meetup.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const MeetupSchema = new mongoose.Schema({
-    name: String,
-    location: String
-})
-
-module.exports = mongoose.model('Meetup', MeetupSchema);
--- Course 5/notes/mongo-connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost/test')
-.then(()=>{
-    console.log('connected yeeii')
-})
-.catch(err => {
-    console.error(err)
-})
